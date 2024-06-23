b36f775455b519a48b897e1b012ef656d8410ccb
gonzalofch <gmfloresch@gmail.com>
2024-06-23 22:23:44 +0200

    Creado un constructor y metodo crearItem para que los test no fallen al cambiar codigo
    Lo que hago aqui es crear una clase createItem que sera utilizada para que en todas las instancias de un Item se pasen valores, no deberia poder instanciarse un Item vacío, esto es propenso a generar errores y bueno en caso de tener un setter, se deberia validar que los valores a los que se hace un set sean validos tambien. No recuerdo como se llama esta tecnica

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -2,6 +2,22 @@
 
 public class Item
 {
+    public Item()
+    {
+    }
+
+    public Item(string name, int sellIn, int quality)
+    {
+        Name = name;
+        SellIn = sellIn;
+        Quality = quality;
+    }
+
+    private static Item CreateItem(string name, int sellIn, int quality)
+    {
+        return new Item(name, sellIn, quality);
+    }
+    
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
