2e6a6094f4835c88eba48977011f79bf8374c55b
gonzalofch <gmfloresch@gmail.com>
2024-06-24 08:12:47 +0200

    dividir ifs en if else completos, y luego sacar las condiciones if de dentro hacia afuera para convertir en un switch case y que se vea mejor que hace en cada caso, de esa manera dejo el else dentro que seria el default del switch case

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace GildedRoseKata;
 
@@ -14,61 +15,62 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        foreach (var item in Items)
+        Items.Select(item =>
         {
             Update(item);
-        }
+            return item;
+        }).ToList();
     }
-    
+
     public void Update(Item item)
     {
-        
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+        if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            if (item.Quality > 0)
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    item.Quality -= 1;
+        }
+        else
+        {
+            if (item.Quality < 50)
             {
-                if (item.Quality > 0)
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        item.Quality -= 1;
+                item.Quality += 1;
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn < 11)
+                        if (item.Quality < 50)
+                            item.Quality += 1;
+
+                    if (item.SellIn < 6)
+                        if (item.Quality < 50)
+                            item.Quality += 1;
+                }
+            }
+        }
+
+        if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn -= 1;
+
+        if (item.SellIn < 0)
+        {
+            //"Aged Brie"
+
+            if (item.Name == "Aged Brie")
+            {
+                if (item.Quality < 50) item.Quality += 1;
+            }
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                item.Quality = 0;
+            }
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                item.Quality -= 0;
             }
             else
             {
-                if (item.Quality < 50)
-                {
-                    item.Quality += 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                            if (item.Quality < 50)
-                                item.Quality += 1;
-
-                        if (item.SellIn < 6)
-                            if (item.Quality < 50)
-                                item.Quality += 1;
-                    }
-                }
+                item.Quality -= 1;
             }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn -= 1;
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                item.Quality -= 1;
-                    }
-                    else
-                    {
-                        item.Quality = 0;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50) item.Quality += 1;
-                }
-            }
+        }
     }
 }
