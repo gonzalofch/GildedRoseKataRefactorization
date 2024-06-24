b8f34baf7383f1a7e8be640edd6b71d908cff745
gonzalofch <gmfloresch@gmail.com>
2024-06-24 15:20:49 +0200

    hacer la computacion para que pasen los tests como antes (restarle un día en el selLin porque ya se restó previamente)

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -30,29 +30,34 @@ public class GildedRose
         //lo que hace el dia anterior
         item.UpdateQuality(item.Name switch
         {
-            "Aged Brie" => 1,
-            "Backstage passes to a TAFKAL80ETC concert" => item.SellIn switch
-            {
-                < 5 => 3,
-                < 10 => 2,
-                _ => 1
-            },
+            "Aged Brie" => item.SellIn < 0
+                ? 2
+                : 1,
+            "Backstage passes to a TAFKAL80ETC concert" => item.SellIn < 0
+                ? -item.Quality
+                : item.SellIn switch //hacer que cuando expiran utilicen otro valor
+                {
+                    < 5 => 3,
+                    < 10 => 2,
+                    _ => 1
+                },
             "Sulfuras, Hand of Ragnaros" => 0,
             // "Conjured Mana Cake" => -2, //doble de rapido xq es creado por mago
-            _ => -1,
+            _ => item.SellIn < 0 ? -2 : -1,
         });
+        
         //pasar esto a un metodo de Item para llamarlo dentro de Update
         //lo que hace para dis
-        
+
         if (item.SellIn < 0)
         {
             item.UpdateQuality(item.Name switch
             {
-                "Aged Brie" => 1,
-                "Backstage passes to a TAFKAL80ETC concert" => -item.Quality,
+                "Aged Brie" => 0,
+                "Backstage passes to a TAFKAL80ETC concert" => 0,
                 "Sulfuras, Hand of Ragnaros" => 0,
                 // "Conjured Mana Cake" => -2, //doble de rapido xq es creado por mago, pero no funciona
-                _ => -1
+                _ => 0,
             });
         }
     }
