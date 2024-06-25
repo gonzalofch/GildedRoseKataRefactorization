287b94fb57f8b1349b8cbe2b2f69b94bf75000ed
gonzalofch <gmfloresch@gmail.com>
2024-06-25 11:14:22 +0200

    refactor de program.cs que me olvide, eliminado Item.cs

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using GildedRoseKata.Items;
 
 namespace GildedRoseKata;
@@ -9,40 +10,38 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("OMGHAI!");
-        
+
         IList<ItemBase> items = new List<ItemBase>
         {
             new NormalItem("+5 Dexterity Vest", 10, 20),
-            new AgedBrie( 2, 0),
+            new AgedBrie(2, 0),
             new NormalItem("Elixir of the Mongoose", 5, 7),
-            new Sulfuras( 0, 80),
-            new Sulfuras( -1, 80),
-            new BackstagePass( 15, 20),
-            new BackstagePass( 10, 49),
-            new BackstagePass( 5, 49),
+            new Sulfuras(0, 80),
+            new Sulfuras(-1, 80),
+            new BackstagePass(15, 20),
+            new BackstagePass(10, 49),
+            new BackstagePass(5, 49),
             // this conjured item does not work properly yet
             new Conjured("Conjured Mana Cake", 3, 6)
         };
-        
+
         var app = new GildedRose(items);
 
-        int days = 2;
-        if (args.Length > 0)
-        {
-            days = int.Parse(args[0]) + 1;
-        }
+        int days = args.Length > 0 ? int.Parse(args[0]) + 1 : 1;
 
-        for (var i = 0; i < days; i++)
+        Enumerable.Range(0, days).ToList().ForEach(day =>
         {
-            Console.WriteLine("-------- day " + i + " --------");
-            Console.WriteLine("name, sellIn, quality");
-            for (var j = 0; j < items.Count; j++)
             {
-                Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
-            }
+                Console.WriteLine("-------- day " + day + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                items.ToList().ForEach(item =>
+                {
+                    Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
+                });
 
-            Console.WriteLine("");
-            app.Update();
-        }
+                Console.WriteLine("");
+                app.Update();
+            }
+        });
     }
 }
